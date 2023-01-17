33d6a4bc382c54ad7d57a2bfea9723c4198d095d
sathit srisawat <>
2023-01-17 09:52:06 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+
+const port = 3500;
+
+const deviceinfo = [];
+
+//show data in device info
+app.get("/", async (req, res) => {
+  res.send(deviceinfo);
+});
+
+// API Post push notification
+app.post("/Pushnotifications", (req, res) => {
+  res.send("successfully");
+});
+
+//Listen port 3000
+server.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
