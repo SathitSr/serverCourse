6d3b73c83607c09e721933a50cebd623a30e87c0
sathit srisawat <>
2023-01-18 16:54:10 +0700

    feat: api

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const util = require("util");
 var mysql = require("mysql");
-
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
 const port = 3500;
 
 var con = mysql.createConnection({
@@ -18,26 +19,71 @@ con.connect(function (err) {
   console.log("Connected!");
 });
 
-//show data in device info
-
-// const testerList = (await testerInfo).map((item) => item.toJSON())
 app.get("/", async (req, res) => {
+  res.send("/api/");
+});
+
+app.get("/api/user", async (req, res) => {
   const rows = await query("SELECT * FROM test.user");
 
   res.send(rows);
 });
 
-//show data in device info
-app.get("/sathit", async (req, res) => {
-  res.send("Hola");
+app.get("/api/get-products", async (req, res) => {
+  const rows = await query("SELECT * FROM test.products");
+  res.send(rows);
 });
 
-// API Post push notification
+//add product
+app.post("/api/add-products", async (req, res) => {
+  const rows = await query(
+    ` INSERT INTO test.products (product_name, product_detail, price_after_discount,price_befor_discount,rate,sold,location,image) VALUES ("${req.body.product_name}", "${req.body.product_detail}", ${req.body.price_after_discount}, ${req.body.price_befor_discount},${req.body.rate},${req.body.sold},"${req.body.location}","${req.body.image}")`
+  );
+  res.send("success");
+});
+
+//check out
+app.post("/api/delete", async (req, res) => {
+  console.log("check body : ", req.body);
+  const rows = await query(`DELETE FROM test.products WHERE id=${req.body.id}`);
+  res.send("success");
+});
+
+//add product to cart
+app.post("/api/add-cart", async (req, res) => {
+  const getProduct = await query(
+    `SELECT * FROM test.products WHERE id = ${req.body.product_id}`
+  );
+
+  if (getProduct) {
+    let productx = JSON.stringify(getProduct);
+    const addCart = await query(
+      ` INSERT INTO test.cart (user_id, product_id, products) VALUES ("${req.body.user_id}", "${req.body.product_id}", ${productx})`
+    );
+  }
+
+  // const addCart = await query(
+  //   ` INSERT INTO test.cart (user_id, product_id) VALUES ("${req.body.user_id}", "${req.body.product_id}")`
+  // );
+  console.log("check getProduct : ", getProduct);
+
+  res.send("success");
+});
+
+//product in cart
+app.get("/api/cart", async (req, res) => {
+  const getCart = await query(
+    `SELECT * FROM test.cart WHERE user_id = ${req.body.user_id}`
+  );
+  console.log("check addcart : ", getCart);
+
+  res.send(getCart);
+});
+
 app.post("/Pushnotifications", (req, res) => {
   res.send("successfully");
 });
 
-//Listen port 3000
 server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
